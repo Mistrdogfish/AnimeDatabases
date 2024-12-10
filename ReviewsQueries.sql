554adc99da0157dc11e95841b9a828b1bcab1628
Ben DeZutti <benjamindezutti@gmail.com>
2024-12-09 23:30:18 -0500

    Delete ReviewsQueries.sql
    No need for this file, duplicate

--- ReviewsQueries.sql
+++
@@ -1,5 +0,0 @@
-Use AnimeDatabase;
-
-Select FirstName AS "First Name", LastName, AnimeReviewBody, AnimeStarRating, AnimeReviewLikeCount FROM Users 
-Join AnimeReviews
-On Users.AnimeReviewerID = AnimeReviews.AnimeReviewerID;
