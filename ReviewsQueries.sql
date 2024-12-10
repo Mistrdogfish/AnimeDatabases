d914571f145d6639404c7d5ab0ace20e29125f2b
evanmarino <157734783+evanmarino@users.noreply.github.com>
2024-12-09 22:36:20 -0500

    Update ReviewsQueries.sql

--- ReviewsQueries.sql
+++ ReviewsQueries.sql
@@ -1,5 +1,16 @@
 Use AnimeDatabase;
 
+DELIMITER $$
+CREATE PROCEDURE DeleteAnimeReview(In anime_id Int, In anime_reviewerid INT)
+
+BEGIN
+	DELETE FROM AnimeReviews
+    WHERE AnimeMatchID = anime_Id AND AnimeReviewerID = anime_reviewerid ;
+END 
+
+
+  DELIMITER ;
+
 SELECT * FROM AnimeReviews
 JOIN Anime
 ON Anime.AnimeID = AnimeReviews.AnimeMatchID; 
