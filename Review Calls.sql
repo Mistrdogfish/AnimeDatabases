88d9f97009be7169dc51ca77ab4c940811e6dc64
evanmarino <157734783+evanmarino@users.noreply.github.com>
2024-12-10 10:08:00 -0500

    Update Review Calls.sql

--- Review Calls.sql
+++ Review Calls.sql
@@ -1,5 +1,28 @@
 Use AnimeDatabase;
 
+DROP PROCEDURE IF EXISTS DeleteAnimeReview;
+DELIMITER $$
+CREATE PROCEDURE DeleteAnimeReview(
+	In anime_id Int,
+    In anime_reviewerid INT
+)
+
+BEGIN
+	DELETE FROM AnimeReviews
+    WHERE AnimeMatchID = anime_Id AND AnimeReviewerID = anime_reviewerid ;
+END
+
+CREATE TRIGGER updateSummary
+AFTER DELETE ON AnimeReview
+BEGIN
+	UPDATE AnimeReviewSummary 
+    SET TotalReviews = TotalReviews - 1,
+		AverageReview = IF(TotalReviews > 0, (SELECT SUM(AnimeStarRating) FROM AnimeReviews WHERE AnimeID = OLD.AnimeID) / Total_Reviews, 0)
+    WHERE AnimeID = OLD.AnimeID;
+END
+	
+DELIMITER ;
+
 #See a singular user's reviews
 CALL ListUserAnimeReview(1);
 CALL ListUserAnimeReview(2);
